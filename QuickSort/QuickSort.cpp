13a4906e82a21a0112fd30d88ada67a3490cdebd
LYChubby <f4uzi345@gmail.com>
2023-03-30 10:49:39 +0700

    Membuat Prosedur Input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -7,3 +7,25 @@ int cmp_count = 0;			// Number of Comparision
 int move_count = 0;			// Number of Data Movements
 int n;
 
+void input() {
+	while (true) {
+
+		cout << "Masukkan Panjang Element Array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum Panjang Array Adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++) {
+
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
